7e43aed54985bbec89f7f449e1acc9415bbe5061
lmnleaf <larkfleming@gmail.com>
2015-01-14 12:29:44 -0700

    makes sound repeat

--- app.js
+++ app.js
@@ -20,68 +20,81 @@ var sound15 = document.getElementsByTagName("audio")[15];
 
 
 $('.sound0').on("click", function() {
-  if(sound0.currentTime != 0) {
-    sound0.currentTime = 0;
-  }
+  sound0.load();
   sound0.play();
 });
 
 $('.sound1').on("click", function() {
+  sound1.load();
   sound1.play();
 });
 
 $('.sound2').on("click", function() {
+  sound2.load();
   sound2.play();
 });
 
 $('.sound3').on("click", function() {
+  sound3.load();
   sound3.play();
 });
 
 $('.sound4').on("click", function() {
+  sound4.load();
   sound4.play();
 });
 
 $('.sound5').on("click", function() {
+  sound5.load();
   sound5.play();
 });
 
 $('.sound6').on("click", function() {
+  sound6.load();
   sound6.play();
 });
 
 $('.sound7').on("click", function() {
+  sound7.load();
   sound7.play();
 });
 
 $('.sound8').on("click", function() {
+  sound8.load();
   sound8.play();
 });
 
 $('.sound9').on("click", function() {
+  sound9.load();
   sound9.play();
 });
 
 $('.sound10').on("click", function() {
+  sound10.load();
   sound10.play();
 });
 
 $('.sound11').on("click", function() {
+  sound11.load();
   sound11.play();
 });
 
 $('.sound12').on("click", function() {
+  sound12.load();
   sound12.play();
 });
 
 $('.sound13').on("click", function() {
+  sound13.load();
   sound13.play();
 });
 
 $('.sound14').on("click", function() {
+  sound14.load();
   sound14.play();
 });
 
 $('.sound15').on("click", function() {
+  sound15.load();
   sound15.play();
 });
