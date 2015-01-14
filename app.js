a466169d053faafbfc5ac345f4331100cc536b0d
lmnleaf <larkfleming@gmail.com>
2015-01-14 12:04:02 -0700

    experimenting with sounds and centering buttons

--- app.js
+++ app.js
@@ -1,8 +1,84 @@
 "use strict";
 
 
-var v = document.getElementsByTagName("audio")[0];
+var sound0 = document.getElementsByTagName("audio")[0];
+var sound1 = document.getElementsByTagName("audio")[1];
+var sound2 = document.getElementsByTagName("audio")[2];
+var sound3 = document.getElementsByTagName("audio")[3];
+var sound4 = document.getElementsByTagName("audio")[4];
+var sound5 = document.getElementsByTagName("audio")[5];
+var sound6 = document.getElementsByTagName("audio")[6];
+var sound7 = document.getElementsByTagName("audio")[7];
+var sound8 = document.getElementsByTagName("audio")[8];
+var sound9 = document.getElementsByTagName("audio")[9];
+var sound10 = document.getElementsByTagName("audio")[10];
+var sound11 = document.getElementsByTagName("audio")[11];
+var sound12 = document.getElementsByTagName("audio")[12];
+var sound13 = document.getElementsByTagName("audio")[13];
+var sound14 = document.getElementsByTagName("audio")[14];
+var sound15 = document.getElementsByTagName("audio")[15];
 
-$('.sound').on("click", function() {
-  v.play();
-})
+
+$('.sound0').on("click", function() {
+  sound0.play();
+});
+
+$('.sound1').on("click", function() {
+  sound1.play();
+});
+
+$('.sound2').on("click", function() {
+  sound2.play();
+});
+
+$('.sound3').on("click", function() {
+  sound3.play();
+});
+
+$('.sound4').on("click", function() {
+  sound4.play();
+});
+
+$('.sound5').on("click", function() {
+  sound5.play();
+});
+
+$('.sound6').on("click", function() {
+  sound6.play();
+});
+
+$('.sound7').on("click", function() {
+  sound7.play();
+});
+
+$('.sound8').on("click", function() {
+  sound8.play();
+});
+
+$('.sound9').on("click", function() {
+  sound9.play();
+});
+
+$('.sound10').on("click", function() {
+  sound10.play();
+});
+
+$('.sound11').on("click", function() {
+  sound11.play();
+});
+
+$('.sound12').on("click", function() {
+  sound12.play();
+});
+
+$('.sound13').on("click", function() {
+  sound13.play();
+});
+
+$('.sound14').on("click", function() {
+  sound14.play();
+});
+
+$('.sound15').on("click", function() {
+  sound15.play();
+});
